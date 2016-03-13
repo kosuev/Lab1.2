b292859504a092e316fc28e1b1e905934ed0478b
kosuev_o <kosuev.o@gmail.com>
2016-03-13 10:52:21 +0200

    Хрєн його знає шо я змінив

--- Афінний шифр/Program.cs
+++ Афінний шифр/Program.cs
@@ -26,9 +26,9 @@ namespace Афінний_шифр
             int a = Convert.ToInt32(Console.ReadLine());
             int b = Convert.ToInt32(Console.ReadLine());
 
-            if ((Evklid(a, 34) != 1)|| (a <= 0) || (a >= 34)||(b<0)||(b>34))
+            while ((Evklid(a, 79) != 1)|| (a >= 0) || (a >= 79)||(b<0)||(b>79))
             {
-                Console.WriteLine("\nВи ввели не вiрнi ключi. \nКлючi мають вiдповiдати таким умовам:\n   0<a<34; \n   0<b<m.\n ");
+                Console.WriteLine("\nВи ввели не вiрнi ключi. \nКлючi мають вiдповiдати таким умовам:\n   0<a<79; \n   0<b<79.\n ");
                 Console.WriteLine("\nВведiть ще раз ключi 'а' i 'b': ");
                 a = Convert.ToInt32(Console.ReadLine());
                 b = Convert.ToInt32(Console.ReadLine());
